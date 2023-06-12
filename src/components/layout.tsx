09fbd4bbae5863688072c311cc5d6e82979c28fa
Daniel Carmona <adlai.carmona@uabc.edu.mx>
2023-06-12 16:17:51 -0700

    :lipstick: Change header background color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -26,7 +26,7 @@ const Layout: React.FunctionComponent<Props> = ({
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex h-[78px] w-full items-center justify-between bg-red-900 px-8  drop-shadow-lg sm:px-32">
+      <header className="flex h-[78px] w-full items-center justify-between px-8  drop-shadow-lg sm:px-32">
         <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
         {/* Place Navbar component here */}
       </header>
