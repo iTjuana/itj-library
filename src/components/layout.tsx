882c3a5c28618c345ef3b1e7a7a66d060429c7c4
Daniel Carmona <adlai.carmona@uabc.edu.mx>
2023-06-12 16:36:20 -0700

    :lipstick: Add header drop shadow & border to footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -26,20 +26,22 @@ const Layout: React.FunctionComponent<Props> = ({
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex h-[78px] w-full items-center justify-between px-8  drop-shadow-lg sm:px-32">
+      <header className="bg- flex h-[78px] w-full items-center justify-between bg-white px-8 drop-shadow-lg sm:px-32">
         <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
         {/* Place Navbar component here */}
       </header>
       {children}
-      <footer className="flex w-full items-center justify-center gap-2.5 py-4">
+      <footer className="flex w-full items-center justify-center gap-2.5 border-t border-[#D4D9E2] bg-white py-4">
         <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
-        <p>© All rights reserved. Copyright 2023. Powered by iTjuana |</p>
+        <p className="text-[#6D6E71]">
+          © All rights reserved. Copyright 2023. Powered by iTjuana |
+        </p>
         <Link
-          className="text-primary underline"
+          className="text-primary no-underline"
           href="https://app.micoach.io"
           target="_blank"
         >
-          <p className=" font-bold">Privacy Policy</p>
+          <p className="font-bold">Privacy Policy</p>
         </Link>
       </footer>
     </>
