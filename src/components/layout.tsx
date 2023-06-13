a8f689940ed5980e1d1a83dfaceaee3633fa6fdf
Daniel Carmona <adlai.carmona@uabc.edu.mx>
2023-06-12 18:13:02 -0700

    :lipstick: Main content will grow to fill layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -26,24 +26,28 @@ const Layout: React.FunctionComponent<Props> = ({
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="bg- flex h-[78px] w-full items-center justify-between bg-white px-8 drop-shadow-lg sm:px-32">
-        <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
-        {/* Place Navbar component here */}
-      </header>
-      {children}
-      <footer className="flex w-full items-center justify-center gap-2.5 border-t border-[#D4D9E2] bg-white py-4">
-        <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
-        <p className="text-[#6D6E71]">
-          © All rights reserved. Copyright 2023. Powered by iTjuana |
-        </p>
-        <Link
-          className="text-primary no-underline"
-          href="https://app.micoach.io"
-          target="_blank"
-        >
-          <p className="font-bold">Privacy Policy</p>
-        </Link>
-      </footer>
+      <div className="flex h-screen w-full flex-col">
+        <header className="flex w-full items-center justify-between bg-white px-8 py-6 drop-shadow-lg sm:px-32">
+          <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
+          {/* Place Navbar component here */}
+        </header>
+
+        <div className="grow">{children}</div>
+
+        <footer className="flex w-full items-center justify-center gap-2.5 border-t border-[#D4D9E2] bg-white py-4">
+          <Image src="/itj_library.svg" alt="itj" width="100" height="32" />
+          <p className="text-[#6D6E71]">
+            © All rights reserved. Copyright 2023. Powered by iTjuana |
+          </p>
+          <Link
+            className="text-primary no-underline"
+            href="https://app.micoach.io"
+            target="_blank"
+          >
+            <p className="font-bold">Privacy Policy</p>
+          </Link>
+        </footer>
+      </div>
     </>
   );
 };
