96a02689b565fa199769ffa75262fa34ab647b9e
Daniel Carmona <adlai.carmona@uabc.edu.mx>
2023-06-13 22:09:24 -0700

    :sparkles: Set up tRPC with dummy endpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,9 @@ import "~/styles/globals.css";
 // fontawesome icon config for css working properly - https://fontawesome.com/docs/web/use-with/react/use-with
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+// tRPC setup from - https://trpc.io/docs/client/nextjs/setup
+import { trpc } from "utils/trpc";
+
 config.autoAddCss = false;
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -20,4 +23,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-export default MyApp;
+export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { trpc } from "utils/trpc";
 
 const Home: NextPage = () => {
+  const hello = trpc.hello.useQuery({ text: "client" });
+  if (!hello.data) console.log("waiting");
+  else console.log(`got: ${hello.data.greeting}`);
   return (
     <>
       <Head>
