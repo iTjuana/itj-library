d11e154deba99f4fa2b0642f133a8248fda6d5c0
NellyQuino <nellyquino999@gmail.com>
2023-06-19 22:24:06 -0700

    :hammer: Refactor Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavLink from "./navlink";
 
 const pages = [
   { name: "Home", href: "/" },
@@ -9,34 +8,13 @@ const pages = [
   { name: "Account", href: "/account" },
 ];
 
-const Navbar: React.FunctionComponent = () => {
-  const pathname = usePathname();
-
+const Navbar = () => {
   return (
     <>
       <nav className="justify-content group flex flex-col items-start gap-4  sm:flex-row sm:gap-16">
-        <Link
-          href="#"
-          className={pathname == "/" ? "text-primary" : "primary-dark"}
-        ></Link>
-        <Link
-          href="#"
-          className={pathname == "/" ? "text-primary" : "primary-dark"}
-        >
-          Catalog
-        </Link>
-        <Link
-          href="#"
-          className={pathname == "/manage" ? "text-primary" : "primary-dark"}
-        >
-          Manage
-        </Link>
-        <Link
-          href="#"
-          className={pathname == "/account" ? "text-primary" : "primary-dark"}
-        >
-          Account
-        </Link>
+        {pages.map((page) => (
+          <NavLink href={page.href}>{page.name}</NavLink>
+        ))}
       </nav>
     </>
   );
