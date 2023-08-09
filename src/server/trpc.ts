d6fef458f3ef6232b52d627beb8eb7fb4f941016
fnespinoza <espinoza.fernandan@gmail.com>
2023-08-08 23:28:29 -0700

    :hammer: change router

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -10,4 +10,3 @@ export const t = initTRPC.context<Context>().create();
 // Base router and procedure helpers
 export const router = t.router;
 export const procedure = t.procedure;
-export const mergeRouters = t.mergeRouters;
