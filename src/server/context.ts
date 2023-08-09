4129d6a21ee429d0e4d69cbad33957932cbc2c51
fnespinoza <espinoza.fernandan@gmail.com>
2023-08-08 22:46:53 -0700

    :sparkles: add context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,14 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-import { Context } from '@trpc/server';
+import { prisma } from "~/server/db";
+import type * as trpc from "@trpc/server";
+import type * as trpcNext from "@trpc/server/adapters/next";
 
-export interface CustomContext extends Context{
-    prisma: PrismaClient;
+export function createContext(ctx: trpcNext.CreateNextContextOptions) {
+  const { req, res } = ctx;
+
+  return {
+    req,
+    res,
+    prisma,
+  };
 }
 
-export function createContext() {
-    const prisma = new PrismaClient();
-
-    return {
-        prisma,
-    };
-}
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,10 +1,11 @@
 import { initTRPC } from "@trpc/server";
+import { type Context } from "./context";
 
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create();
+export const t = initTRPC.context<Context>().create();
 
 // Base router and procedure helpers
 export const router = t.router;
