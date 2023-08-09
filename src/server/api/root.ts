c4c055970a04a39fa4736fe13734c159db5c9b14
fnespinoza <espinoza.fernandan@gmail.com>
2023-08-09 16:50:38 -0700

    :zap: add books router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { inventoryRouter } from "./routers/inventory";
+import { booksRouter } from "./routers/books";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { inventoryRouter } from "./routers/inventory";
  */
 export const appRouter = createTRPCRouter({
   inventory: inventoryRouter,
+  books: booksRouter,
 });
 
 // export type definition of API
