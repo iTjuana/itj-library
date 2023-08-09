01679a1c8faad33bfe0b65bea65298972fb2e9e3
Daniel Carmona <adlai.carmona@uabc.edu.mx>
2023-08-09 10:39:47 -0700

    :zap: Improve trcp files

---
+++ src/server/api/routers/inventory.ts
@@ -0,0 +1,61 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { type Inventary, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function getFullInventory(): Promise<Inventary[] | undefined> {
+  try {
+    return await prisma.inventary.findMany();
+  } catch (error) {
+    console.error("Error findMany inventory", error);
+  }
+}
+
+export const inventoryRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      })
+    )
+    .query((opts) => {
+      return {
+        greeting: `hello ${opts.input.text}`,
+      };
+    }),
+  inventory: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(25).optional(),
+        availability: z.number().optional(),
+        format: z.number().optional(),
+        language: z.number().optional(),
+        page: z.number(),
+      })
+    )
+    .query((opts) => {
+      const { input } = opts;
+      const { page, availability, format } = input;
+      const limit = input.limit ?? 25;
+      console.log(input);
+      return prisma.inventary.findMany({
+        take: limit,
+        where: {
+          status: availability?.toString(),
+          format: format?.toString(), // need to change to number
+        },
+        skip: (page - 1) * limit,
+      });
+    }),
+  count: publicProcedure.query(() => {
+    return prisma.inventary.count();
+  }),
+});
+
+// export type definition of API
+export type InventoryRouter = typeof inventoryRouter;
