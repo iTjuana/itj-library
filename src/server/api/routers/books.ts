a5cce283b5cbd5436229e32ee866ab5800ce9e61
fnespinoza <espinoza.fernandan@gmail.com>
2023-08-10 09:24:49 -0700

    :zap: findBookById

--- src/server/api/routers/books.ts
+++ src/server/api/routers/books.ts
@@ -9,7 +9,7 @@ import { type Book, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const bookInput = z.object({
-  idISBN: z.string(),
+  idISBN: z.string().optional(),
   isbn: z.string(),
   title: z.string(),
   subtitle: z.string(),
@@ -21,6 +21,7 @@ const bookInput = z.object({
   publishers: z.string(),
   number_of_pages: z.string(),
   image: z.string(),
+  inventary: z.string().optional(),
 });
 
 async function getBooksTable(): Promise<Book[] | undefined> {
@@ -35,15 +36,41 @@ export const booksRouter = createTRPCRouter({
   // Get all books
   getBooks: publicProcedure
   .query(async () => {
+    console.log( await prisma.book.findMany());     // TODO: Remove line
     return await prisma.book.findMany();
   }),
 
-  // Add Books
+  // Find Book
+  findBookById: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query( async( opts ) => {
+      // Logic to find book by id
+      const { input } = opts;
+      const findBook = await prisma.book.findUnique({
+        where: {
+          idISBN: input.id,
+        }
+      })
+      console.log("yayyy");                         // TODO: Remove line
+      console.log(findBook);                        // TODO: Remove line
+
+    }),
+
+  // Add Book
   addBook: publicProcedure // TODO: Change to privateProcedure?
   .input(bookInput)
-  .mutation(async ({ input, ctx }) =>{
+  .mutation(async ( opts ) => {
     // Logic to add books
-    return await ctx.prisma.book.create({data: input});
+    const { input } = opts;
+    console.log('Book added');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    console.log("Show Input");
+    console.log(input);
+    // return await prisma.book.create({ data: input});
   }),
   
   // TODO: Confirm if needed
@@ -52,11 +79,6 @@ export const booksRouter = createTRPCRouter({
       session: "Session",
     };
   }),
-
-  // Find Book
-  // findBookById: publicProcedure
-  //   .input(bookInput)
-  //   .query(),
 
   // Update Book
   // updateBook: publicProcedure // TODO: Change to privateProcedure
