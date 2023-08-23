bafa8b37f540a238656783153147582aad2f5efb
Jordi <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2023-08-22 19:46:40 -0700

    Added Carousel with React Slick library (#71)
    * fix: resolve conflicts
    
    * fix: updated carousel data at books
    
    * fix: updated carousel props
    
    * fix: remove unused changes

---
+++ src/components/carousel.tsx
@@ -0,0 +1,97 @@
+import Slider from "react-slick";
+import Link from "next/link";
+import Image from "next/image";
+
+const settings = {
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
+export interface CarouselProps {
+  books: {
+    title: string;
+    image: string;
+  }[];
+  size?: "small" | "medium" | "large";
+  dots?: boolean;
+  speed?: number;
+  slidesToShow?: number;
+  slidesToScroll?: number;
+  infinite?: boolean;
+}
+
+export const SimpleCarousel: React.FunctionComponent<CarouselProps> = (
+  props
+) => {
+  const { books, size, dots, speed, slidesToShow, slidesToScroll, infinite } =
+    props;
+
+  const width = size === "small" ? 200 : size === "medium" ? 300 : 350;
+  const height = size === "small" ? 200 : size === "medium" ? 300 : 350;
+
+  if (!books?.length) {
+    return <></>;
+  }
+
+  return (
+    <div>
+      <Slider
+        {...settings}
+        dots={dots}
+        speed={speed}
+        slidesToShow={slidesToShow}
+        slidesToScroll={slidesToScroll}
+        infinite={infinite}
+      >
+        {books?.map((book) => (
+          <div
+            className="!flex flex-col items-center justify-center"
+            key={book?.title}
+          >
+            <Link href={book?.title ?? "/"}>
+              <Image
+                src={book.image ?? "/cover-unavailable.jpg"}
+                width={width}
+                height={height}
+                alt="cover"
+              />
+            </Link>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+SimpleCarousel.defaultProps = {
+  size: "medium",
+  dots: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,11 @@ import "~/styles/globals.css";
 // fontawesome icon config for css working properly - https://fontawesome.com/docs/web/use-with/react/use-with
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+// slick carousel css
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "~/styles/carousel.css";
+
 // tRPC setup from - https://trpc.io/docs/client/nextjs/setup
 import { api } from "utils/trpc";
 import Layout from "~/components/layout";
