f343dfd5d83710636eb380b315858bb23ad568e0
Daniel Carmona <adlai.carmona@uabc.edu.mx>
2023-11-30 23:34:20 -0800

    :bug: use session query

--- utils/session.ts
+++ utils/session.ts
@@ -4,9 +4,7 @@ import { api } from "utils/trpc";
 export const getUserId = () => {
   const { data: session, status } = useSession();
 
-  const user = api.users.findUserByEmail.useQuery({
-    email: session?.user.email ?? "0",
-  }).data;
+  const user = api.users.getUserBySession.useQuery().data;
 
   return user?.id ?? "0";
 };
